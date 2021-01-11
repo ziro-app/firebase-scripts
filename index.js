34a9dd2ada358f7a0ee863a691a851dcea061544
Wermeson Rocha <wermesonrocha2@gmail.com>
2021-01-11 19:08:58 -0300

    Added functions to correct errors in writing transactions in firebase and spreadsheets

--- index.js
+++ index.js
@@ -6,9 +6,82 @@ admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
 const db = admin.firestore()
 
 const removeDuplicates = require('@ziro/remove-duplicates')
-const { formatDateUTC } = require('@ziro/format-date-utc3')
+const { formatDateUTC, formatDateUTC3 } = require('@ziro/format-date-utc3')
 const currencyFormat = require('@ziro/currency-format')
 const axios = require('axios')
+
+const formatHour = time => {
+	const [dateHour, period] = time.split(' ');
+	const [hour, minute, second] = dateHour.split(':');
+	if (period === 'PM') return (parseInt(hour) === 12) ? `${hour}:${minute}:${second}` : `${parseInt(hour) + 12}:${minute}:${second}`;
+	else return (parseInt(hour) === 12) ? `00:${minute}:${second}` : `${parseInt(hour) <= 9 ? `0${hour}` : hour}:${minute}:${second}`;
+};
+
+const formatDate = utcDate => {
+	const [date, hour] = formatDateUTC3(utcDate).split(', ');
+	const _hour = formatHour(hour);
+	const [mounth, day, year] = date.replace(',', '').split('/');
+	return `${parseInt(day) <= 9 ? `0${day}` : day}/${parseInt(mounth) <= 9 ? `0${mounth}` : mounth}/${year} ${_hour}`;
+};
+
+// Situação -> Transação passou na Zoop e não atualizou nas 2 bases (Com seguro)
+const updateFirebase = async () => await db.collection('credit-card-payments').doc('##docID##').update({
+	status: "Status_Transacao",
+	installments: "Nº parcelas",
+	datePaid: new Date("Data/Hora_Pagamento"), // Formato -> AAAA-MM(1-12)-DD HH:MM:SS
+	dateLastUpdate: new Date("Data/Hora_Pagamento"), // Formato -> AAAA-MM(1-12)-DD HH:MM:SS
+	cardBrand: "Bandeira_Cartao", // capitalizado -> Visa,MasterCard, etc
+	cardholder: "Portador_cartao", // em lowerCase()
+	cardFirstFour: "****",
+	cardLastFour: "****",
+	transactionZoopId: "Id_Transacao",
+	receiptId: "Id_receipt", // -> campo sales_receipt, encontrado nos detalhes da transação (Zoop)
+	splitTransaction: {
+		// split_rules da transacao -> Fazer o match com o firebase
+		antiFraud: { amount: 0, percentage: 0.95 },
+		markup: { amount: 0, percentage: 0 },
+	},
+	authorizer: "rede",
+	onBehalfOfBrand: "",
+	buyerStoreownerId: "Doc_Id_Comprador",
+	buyerRazao: "Razao_Pagador"
+});
+
+const updateSheet = async () => {
+	const url = process.env.SHEET_URL;
+	const config = {
+		headers: {
+			'Content-type': 'application/json',
+			'Authorization': process.env.SHEET_TOKEN,
+			'Origin': 'https://ziro.app'
+		}
+	};
+	const body = {
+		apiResource: 'values',
+		apiMethod: 'append',
+		spreadsheetId: process.env.SHEET_TRANSACTIONS_ID,
+		range: 'Transacoes!A1',
+		resource: {
+			values: [
+				[
+					"Id_Transacao",
+					formatDate(new Date("2021-01-11T13:09:40+00:00")),
+					"Status_Transacao",
+					"crédito",
+					"Nº parcelas",
+					"Hut Confeccoes - Eireli",
+					"Razao_Pagador",
+					"Portador_cartao", // em lowerCase()
+					"Bandeira_Cartao", // capitalizado -> Visa,MasterCard, etc
+					`****...****`,
+					"2.591,39"
+				]
+			]
+		},
+		valueInputOption: 'user_entered'
+	};
+	await axios.post(url, body, config);
+};
 
 const getAllPayments = async () => {
 	const cardPayments = await db.collection('credit-card-payments').get()
@@ -24,28 +97,28 @@ const getAllSuppliersWithPayments = async () => {
 }
 
 const getAllReceivables = async () => {
-	const cardPayments = await db.collection('credit-card-payments').where('status','==','Aprovado').get()
+	const cardPayments = await db.collection('credit-card-payments').where('status', '==', 'Aprovado').get()
 	let receivables = []
 	cardPayments.forEach(doc => receivables.push({
 		id: doc.data().transactionZoopId,
 		receivables: doc.data().receivables
 	}))
 	const formatted = receivables.map(({ id, receivables: arrReceivable }) =>
-			arrReceivable.map(({ installment, gross_amount, amount, expected_on, paid_at, status }) => {
-			const grossAmount = gross_amount.replace('.',',')
-			const finalAmount = amount.replace('.',',')
+		arrReceivable.map(({ installment, gross_amount, amount, expected_on, paid_at, status }) => {
+			const grossAmount = gross_amount.replace('.', ',')
+			const finalAmount = amount.replace('.', ',')
 			const dateExpected = expected_on ? formatDateUTC(new Date(expected_on._seconds * 1000)) : ''
 			const [expected] = typeof dateExpected === 'string' ? dateExpected.split(' ') : ''
 			const datePaid = paid_at ? formatDateUTC(new Date(paid_at._seconds * 1000)) : ''
 			const [paid] = typeof datePaid === 'string' ? datePaid.split(' ') : ''
 			const translatedStatus = status.toLowerCase() === 'pending' ? 'Pendente' : 'Pago'
 			return [id, installment, grossAmount, finalAmount, expected, paid, translatedStatus]
-	}))
+		}))
 	console.log(formatted.flat())
 }
 
 const getAllPaymentsToDelete = async () => {
-	const cardPayments = await db.collection('credit-card-payments').where('seller','==','Lojas Marisa').get()
+	const cardPayments = await db.collection('credit-card-payments').where('seller', '==', 'Lojas Marisa').get()
 	console.log(cardPayments)
 	let payments = []
 	cardPayments.forEach(doc => payments.push(doc.ref.delete()))
@@ -76,21 +149,26 @@ const fetchZoopEvents = async () => {
 	const data = await fetch(dateMin, dateMax)
 	const { items, total } = data
 	const numberOfRequests = Math.ceil(total / 100)
-	console.log('total requests:',numberOfRequests)
-	console.log('total items:',total)
+	console.log('total requests:', numberOfRequests)
+	console.log('total items:', total)
 	if (data.has_more) {
 		for (let i = 0; i < Math.ceil(data.total / 100) - 1; i++) {
 			const { items: items_more } = await fetch(dateMin, dateMax, 100 + i * 100)
 			items.push(...items_more)
-			console.log('items mapped:',items.length)
+			console.log('items mapped:', items.length)
 		}
-		const events = items.map(({ type }) => type).sort((a,b) => a < b ? -1 : 1)
+		const events = items.map(({ type }) => type).sort((a, b) => a < b ? -1 : 1)
 		console.log(removeDuplicates(events))
 	} else {
-		console.log('items mapped:',items.length)
-		const events = items.map(({ type }) => type).sort((a,b) => a < b ? -1 : 1)
+		console.log('items mapped:', items.length)
+		const events = items.map(({ type }) => type).sort((a, b) => a < b ? -1 : 1)
 		console.log(removeDuplicates(events))
 	}
 }
 
-fetchZoopEvents()
+fetchZoopEvents();
+
+// Funções para corrigir erros de escrita no firebase e planilhas
+// updateFirebase();
+// updateSheet();
+// -> Após rodar essas funções, lembrar de rodar o webhook transaction
